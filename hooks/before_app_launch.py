aad0a35ad7c53a30fd2b3cbbeca75b6d638e8915
mgidney <mgidney@gmail.com>
2017-11-28 17:33:39 +1100

    test add xxx

--- hooks/before_app_launch.py
+++ hooks/before_app_launch.py
@@ -29,7 +29,7 @@ else:
 #######################
 
 
-# 
+#
 class BeforeAppLaunch(tank.Hook):
     """
     Hook to set up the system prior to app launch.
@@ -37,18 +37,18 @@ class BeforeAppLaunch(tank.Hook):
 
     def execute(self, app_path, app_args, version, **kwargs):
         """
-        The execute functon of the hook will be called prior to starting the required application        
+        The execute functon of the hook will be called prior to starting the required application
 
         :param app_path: (str) The path of the application executable
         :param app_args: (str) Any arguments the application may require
         :param version: (str) version of the application being run if set in the "versions" settings
                               of the Launcher instance, otherwise None
-                              
-                              
-                              
-        If you use environment variables in full paths, these will be automatically 
-        expanded. Note that on linux, they must be on the $ENV_VAR form and not on 
-        the ${ENV_VAR} form as this will confuse the include parser. Example: 
+
+
+
+        If you use environment variables in full paths, these will be automatically
+        expanded. Note that on linux, they must be on the $ENV_VAR form and not on
+        the ${ENV_VAR} form as this will confuse the include parser. Example:
         includes: ['$STUDIO_ROOT/foo/bar/hello.yml', '%STUDIO_ROOT%\foo\bar\hello.yml']
 
         """
@@ -60,11 +60,11 @@ class BeforeAppLaunch(tank.Hook):
         # > current_entity = multi_launchapp.context.entity
         # you can set environment variables like this:
         # DABRENDER,DABWORK,DABUSR,DABASSETS,DABUSERPREFS ARE DEFINED EXTERNALLY
-        
+
         _db   ="/Volumes/dabrender"
         _dbdev="/Users/Shared/UTS_Dev/gitRepositories/utsdab_repos"
-        
-                            	
+
+
         ENV={
             "DABRENDER"         : "{}".format(_db),\
             "DABWORK"           : "{}/work".format(_db),\
@@ -76,6 +76,7 @@ class BeforeAppLaunch(tank.Hook):
             "CONFIG"            : "{}/work/user_prefs/{}/config".format(_db,os.environ["USER"]),\
             "RMAN_VERSION"      : "21.5",\
             "MAYA_VERSION"      : "2017",\
+            "XXX"               : "testing only",\
             "TRACTOR_VERSION"   : "2.2",\
             "SHOTGUNENV_SOURCE" : "before_app_launch",\
             "SHOTGUNENV_USER"   : "{}".format(os.environ["USER"])
@@ -85,16 +86,16 @@ class BeforeAppLaunch(tank.Hook):
         for key in ENV.keys():
             os.environ[key] = ENV[key]
             tank.util.append_path_to_env_var(key, ENV[key])
-        
+
 
         # if you are using a shared hook to cover multiple applications,
-        # you can use the engine setting to figure out which application 
+        # you can use the engine setting to figure out which application
         # is currently being launched:
         #
         multi_launchapp = self.parent
 
         if multi_launchapp.get_setting("engine") == "tk-maya":
-        
+
             ###############################  ONLY INTERESTED IN OSX
             ENV_MAYA={\
             "MAYA_APP_DIR"         : "{}/mayaprefs".format(os.environ["CONFIG"]),\
@@ -108,7 +109,7 @@ class BeforeAppLaunch(tank.Hook):
             for key in ENV_MAYA.keys():
                 os.environ[key] = ENV_MAYA[key]
                 tank.util.prepend_path_to_env_var(key, ENV_MAYA[key])
-            
+
             ##################################    ONLY INTERESTED IN OSX
             ENV_NUKE={\
             "RMANTREE"            : "/Applications/Pixar/RenderManProServer-{}".format(os.environ["RMAN_VERSION"]),\
@@ -118,9 +119,9 @@ class BeforeAppLaunch(tank.Hook):
             for key in ENV_NUKE.keys():
                 os.environ[key] = ENV_NUKE[key]
                 tank.util.append_path_to_env_var(key, ENV_NUKE[key])
-        
+
             # If there is a Task in the context, set its status to 'ip'
-		
+
         if self.parent.context.task:
             task_id = self.parent.context.task['id']
             data = {
@@ -131,5 +132,5 @@ class BeforeAppLaunch(tank.Hook):
 
 
 
-    
+
 
